5ad392c67e4d2414afec93287d29c0a33fb2e50a
Kyra Shelwo <kyrashelwo@gmail.com>
2016-01-08 12:45:25 +0100

    resizing works now also for blocks of width 1

--- resizeIlp.py
+++ resizeIlp.py
@@ -3,6 +3,7 @@ import numpy as np
 from shutil import copyfile
 from h5py import File
 import os
+from IPython import embed
 
 from libs.essentials import splitPath
 
@@ -10,7 +11,8 @@ from IlastikLabelManager import labelManager
 
 def downsampleProject(ilpPath, adjustScales=False, resizeRaw=True):
     assert ilpPath.endswith('.ilp'), 'ilp file is required for downsampling. did you mean to upsample?'
-    name = ilpPath + 'small.ilp'
+    name = ilpPath.split('.ilp')[0] + '_small.ilp'
+    print 'copy original to', name
     copyfile(ilpPath, name)
 
     f = File(name)
@@ -25,18 +27,25 @@ def downsampleProject(ilpPath, adjustScales=False, resizeRaw=True):
 
 
     for key in f['Input Data/infos'].keys():
-        path = f['Input Data/infos/' + key + '/Raw Data/filePath'].value
+        path = splitPath(f['Input Data/infos/' + key + '/Raw Data/filePath'].value)
 
         # resize the labels
         labels = labelManager(name)
         labeledBlocks = labels.getSubBlocks()
         labeledBlocksResized = list()
         for labeledBlock in labeledBlocks:
+            labeledBlock[1] = labeledBlock[1][...,0]
+            # size must be at least 4x4x4 to work without an error.
+            shapeOrig = labeledBlock[1].shape[:3]
+            shapeTarget = [max(int(ordinate/2), 1) for ordinate in shapeOrig]
+            labeledBlock[1] = np.resize(labeledBlock[1], [max(ordinate,4) for ordinate in labeledBlock[1].shape])
             targetShape = tuple([int(ordinate / 2) for ordinate in labeledBlock[1].shape[:3]])
             resized = np.zeros(targetShape, dtype=np.uint8)
-            vigra.graphs.downsampleLabels(labeledBlock[1], int(labelBlockTotal.max()), 0.1, resized)
-            offsetResized = tuple([int(ordinate/2) for ordinate in labeledBlock[0])
-            labeledBlocksResized.append((offsetResized, resized)
+            vigra.graphs.downsampleLabels(labeledBlock[1].astype(np.uint8), int(labeledBlock[1].max()), 0.05, resized) # get back the original shape
+
+            resized = resized[:shapeTarget[0], :shapeTarget[1], :shapeTarget[2]]
+            offsetResized = tuple([int(ordinate/2) for ordinate in labeledBlock[0]])
+            labeledBlocksResized.append((offsetResized, resized))
 
         # clear the old labelds
         labels.clear()
@@ -48,12 +57,13 @@ def downsampleProject(ilpPath, adjustScales=False, resizeRaw=True):
         labels.flush()
 
         if resizeRaw:
-            print 'resize raw data'
-            rawData = vigra.readHDF5(*rawPath)
+            print 'resize raw data in', path
+            rawData = vigra.readHDF5(*path)
+            resizedShape = [int(ordinate/2) for ordinate in rawData.shape]
 
-            exportRawPath = [rawPath[0].split('.')[0] + '_resized.h5', rawPath[1]]
+            exportRawPath = [path[0].split('.')[0] + '_resized.h5', path[1]]
 
-            rawSmall = vigra.sampling.resize(rawData.astype(np.float32), resizedShape, 0)
+            rawSmall = vigra.sampling.resize(rawData.astype(np.float32), resizedShape , 0)
             vigra.writeHDF5(rawSmall, *exportRawPath)
 
 
@@ -99,27 +109,32 @@ def pureConcaternation(probPath, rawPath):
 
     assert len(splitPath(probPath)) == 2, "file " + probPath + "seems to not have the .h5 extension"
 
+    print 'load prob data'
     data = vigra.readHDF5(*splitPath(probPath)).squeeze()
 
     if len(data.shape) != 4:
         print "WARNING: untested for data other than 3d + channel."
 
-    if isinstance(data,vigra.VigraArray):
-        data = data.view(np.ndarray)
+    # if isinstance(data,vigra.VigraArray):
+        # data = data.view(np.ndarray)
 
+    print 'normalize prob data'
     # normalize probabilities and save them as hdf5
     data = np.require(data, dtype=np.float32)
-    data = vigra.sampling.resize(data, shape=[size*2 for size in data.shape[:-1]], order=2)
     data -= data.min()
     data *= 255 / data.max()
     data = data.astype(np.uint8)
 
     if rawPath != None:
+        print 'load raw data'
         assert len(splitPath(rawPath)) == 2, "file " + rawPath + "seems to not have the .h5 extension"
         raw = vigra.readHDF5(*splitPath(rawPath)).squeeze()
+
+        print 'concaternate'
+        embed()
         data = np.concatenate((raw[:,:,:,None], data), axis=3)
 
-    vigra.writeHDF5(data.astype(np.uint8), splitProbPath[0] + '_upsampled.h5', splitProbPath[1])
+    vigra.writeHDF5(data.astype(np.uint8), splitPath(probPath)[0] + '_stacked.h5', splitPath(probPath)[1])
 
 
 
@@ -130,13 +145,14 @@ if __name__ == '__main__':
     parser.add_argument("--stackWithRaw", default=None)
     parser.add_argument("--upsample", action="store_true")
     parser.add_argument("--adjustScales", action="store_true")
+    parser.add_argument("--ignoreRaw", default=False, action="store_true")
 
     args = parser.parse_args()
     path = args.input_path
     if args.upsample:
         upsample(path, args.stackWithRaw)
     elif args.stackWithRaw is not None:
-        pureConcaternation(probPath, rawPath)
+        pureConcaternation(args.input_path, args.stackWithRaw)
     else:
-        downsampleProject(path, args.adjustScales)
+        downsampleProject(path, args.adjustScales, not args.ignoreRaw)
     # upsampleMultiChannelData('/home/timo/multiscaletest/results/actualHoles_resized_probs.h5', 'exported_data')
